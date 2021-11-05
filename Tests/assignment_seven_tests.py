6ce201bb165d36e67ee83a8ced72c7e08a8fcca0
wilkinsonbrian <wilkibrian@gmail.com>
2021-11-05 08:13:33 -0400

    Completed Assignment Seven Tests file

--- Tests/assignment_seven_tests.py
+++ Tests/assignment_seven_tests.py
@@ -3,8 +3,15 @@ import assignment_seven
 
 
 class MyTestCase(unittest.TestCase):
-    def test_encode(self):
+    def test_encode_one(self):
         self.assertEqual("sbwkrq", assignment_seven.encode("python", 3))
+
+    def test_encode_two(self):
+        self.assertEqual("wcescjdpek", assignment_seven.encode("hello world", "python"))
+
+    def test_option_three(self):
+        pass
+
 
 
 if __name__ == '__main__':
