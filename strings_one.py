04eea7dc020c151b60e858c463d554868bd37091
twig <willitw@ssfs.org>
2023-12-06 08:20:29 -0500

    some things

--- strings_one.py
+++ strings_one.py
@@ -1,16 +1,35 @@
 def half_slice(word):
-    pass
 
+    midpoint = len(word) // 2
 
-def no_first_last(str):
-    pass
+    first_half = word[:midpoint]
+    second_half = word[midpoint:]
+
+    new_word = second_half + first_half
+
+    return new_word
+
+def no_first_last(some_str):
+  pass
 
 
 def longest(phrase):
-    pass
+   pass
+
 
 
 def title_case(sentence):
-    pass
+    words = sentence.split()
+    title_case_words = []
+    for word in words:
+        capitalized_word = word[0].upper() + word[1:]
+        title_case_words.append(capitalized_word)
+    title_case_sentence = " ".join(title_case_words)
+    return title_case_sentence
 
 
+print(half_slice("bread"))
+
+user_input = input("Enter a sentence: ")
+result = title_case(user_input)
+print(result)
--- upper_lower.py
+++ upper_lower.py
@@ -1,7 +1,10 @@
 
+def upper_lower(word):
+    print("Upper case:", word.upper())
+    print("Lower case:", word.lower())
 
 def main():
-    pass
+    user_input = input("Enter a word: ")
+    upper_lower(user_input)
 
-if __name__ == '__main__':
-    main()
+main()
