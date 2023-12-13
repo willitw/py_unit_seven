6f7409a9f7ce2f378aeb92d19287e44be452c1c1
twig <willitw@ssfs.org>
2023-12-13 08:52:23 -0500

    Changes by liam

---
+++ assignment_seven.py
@@ -0,0 +1,108 @@
+def begin():
+    print("welcome")
+    print("clarify HP and how u die")
+
+def you_die():
+    play_again = input("would you like to play again? y or n")
+    if play_again = "y":
+        begin()
+    if play_again = "n":
+        print("we'll be here waiting when you change your mind")
+    if play_again != "y"
+
+def choiceA1():
+    print("you find a stick, a sharp rock, a rag, and a lighter")
+
+def choiceA2():
+    print("you can still smell food")
+
+def choiceA3():
+    print("you encounter a giant cave spider busy wrapping what looks like a boar carcass in webs. its eyes are cloudy with disuse (it is blind)")
+
+def choiceA4():
+    print("you successfully kill the spider with your spear but you are injured, you take off a piece of the boar")
+
+def choiceA5():
+    print("you are no longer hungry and have a rush of energy")
+
+def choiceB1():
+    print("you encounter a giant cave spider busy wrapping what looks like a boar carcass in webs. its eyes are cloudy with disuse (it is blind)")
+
+def choiceB2():
+    ("it hears you and kills you")
+
+def choiceC1():
+    print("the light from the torch attracts a bunch of cave bugs. they start to climb you to try to get to the light")
+
+def choiceC2():
+    print("they climb up you and jump into the fire, making it bigger you are burt and hurt, but your torch is now extra bright. it allows you to see a patch of shimmery looking moss ")
+
+def choiceC3():
+    print("you get high and doze off to sleep")
+
+
+def choiceD1():
+    print("they bite you when you brush them off. they are venomous and you are poisoned")
+
+def choiceD2():
+    print("don't eat moss and remain hungry, you eventuly starve")
+
+def choiceE1():
+    print("your hungry stomach makes a load noise that atracts something you cant see that kills you")
+
+def choiceF1():
+    print("your spear makes a noise as you move. it hears you and kills you")
+
+def choiceG1():
+    print("your spear makes a noise as you move. it hears you and kills you")
+
+def choiceH1():
+    pass
+
+def choiceH2():
+    pass
+
+def choiceH3():
+    pass
+
+def choiceI1():
+    pass
+
+def choiceJ1():
+    pass
+
+def choiceK1():
+    pass
+
+def choiceL1():
+    pass
+
+def choiceL2():
+    pass
+
+def choiceL3():
+    pass
+
+def choiceL4():
+    pass
+
+def choiceM1():
+    pass
+
+def choiceN1():
+    pass
+
+def choiceO1():
+    pass
+
+def choiceP1():
+    pass
+
+def choiceP2():
+    pass
+
+def choiceQ1():
+    pass
+
+def choiceeR1():
+    pass
