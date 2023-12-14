94e400e4d24d8e9a22ac5fc456cbd53c3a7e31d8
twig <willitw@ssfs.org>
2023-12-14 12:02:00 -0500

    some things

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,12 +1,11 @@
 def begin():
-    print("welcome")
-    print("clarify HP and how u die")
+
 
 def you_die():
     play_again = input("would you like to play again? y or n")
-    if play_again = "y":
+    if play_again == "y":
         begin()
-    if play_again = "n":
+    if play_again == "n":
         print("we'll be here waiting when you change your mind")
     if play_again != "y"
 
@@ -23,7 +22,7 @@ def choiceA4():
     print("you successfully kill the spider with your spear but you are injured, you take off a piece of the boar")
 
 def choiceA5():
-    print("you are no longer hungry and have a rush of energy")
+    print("you are no longer hungry and have a rush of energy, you keep venturing on and find the exit")
 
 def choiceB1():
     print("you encounter a giant cave spider busy wrapping what looks like a boar carcass in webs. its eyes are cloudy with disuse (it is blind)")
@@ -35,7 +34,7 @@ def choiceC1():
     print("the light from the torch attracts a bunch of cave bugs. they start to climb you to try to get to the light")
 
 def choiceC2():
-    print("they climb up you and jump into the fire, making it bigger you are burt and hurt, but your torch is now extra bright. it allows you to see a patch of shimmery looking moss ")
+    print("they climb up you and jump into the fire, making it bigger you are burt and hurt, but your torch is now extra bright. it allows you to see a patch of shimmery looking moss")
 
 def choiceC3():
     print("you get high and doze off to sleep")
@@ -57,52 +56,58 @@ def choiceG1():
     print("your spear makes a noise as you move. it hears you and kills you")
 
 def choiceH1():
-    pass
+    print("you store the meat for later")
 
 def choiceH2():
-    pass
+    print("you smell like food and something you can't see attacks you are severely injured but alive")
 
 def choiceH3():
-    pass
+    print("it eats you and your boar drumstick")
+    you_die()
 
 def choiceI1():
-    pass
+    print("you keep walking forward but get hungry you try to eat your raw meat you saved but die from food poisoning")
+    you_die()
 
 def choiceJ1():
-    pass
+    print("it eats you and your boar drumstick")
+    you_die()
 
 def choiceK1():
-    pass
+    print("you are unarmed. it kills you")
+    you_die()
 
 def choiceL1():
-    pass
+    print("you successfully sneak past the spider but you hear something behind you")
 
 def choiceL2():
-    pass
+    print("you see three pathways you must choose one to go down")
 
 def choiceL3():
-    pass
-
-def choiceL4():
-    pass
+    print("dead end you get killed")
+    you_die()
 
 def choiceM1():
-    pass
+    print("you step on an old scapula bone. it hears you and kills you")
+    you_die()
 
 def choiceN1():
-    pass
+    print("it smells you and deems you delicious")
+    you_die()
 
 def choiceO1():
-    pass
+    print("you find the exit to the cave")
 
 def choiceP1():
-    pass
+    print("you trip and and get very hurt, your are running away scared and find 3 more pathways")
 
 def choiceP2():
-    pass
+    print("you find the exit to the cave")
 
 def choiceQ1():
-    pass
+    print("dead end you get killed ")
+    you_die()
 
 def choiceeR1():
-    pass
+    print("you trip and get eaten")
+    you_die()
