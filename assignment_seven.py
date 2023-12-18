1bbaa6fc399faa13fc3110528533b5f9a772f537
twig <willitw@ssfs.org>
2023-12-18 14:38:31 -0500

    some things

--- assignment_seven.py
+++ assignment_seven.py
@@ -3,17 +3,19 @@
 # 12/15/23
 # This program plays a text adventure game, that based in a cave where you must make correct choices to find the exit
 
+#
 
 def you_die():
     '''
     This function prints a message indicating that the player has died and prompts them to play again.
     :return: None
     '''
+    print(" ")
     print("You died, better luck next time!")
     while True:                                                             # Infinite loop to keep prompting the player until valid input is received.
         play_again = input("Would you like to play again? Input y or n:")   # Prompting the player to choose whether to play again.
         if play_again == "y":                                               # If the player chooses to play again,
-            start_game()                                                    # Restart the game by calling the start_game function.
+            prob_wakeup_A1_B1()                                             # Restart the game by calling the first problem function.
             exit()                                                          # Exit the current function when start_game() is called.
         if play_again == "n":                                               # If the player chooses not to play again,
             print("We'll be here waiting when you change your mind.")       # Display a message indicating the game is ready when the player decides to play again.
@@ -25,12 +27,13 @@ def you_win():
     This function prints a message indicating that the player has won and prompts them to play again.
     :return: None
     '''
+    print(" ")
     print("You turn a corner and have to sheild your eyes from the glaring sunlight.")
     print("You have successfully survived the cave.")
     while True:
         play_again = input("Would you like to play again? Input y or n:")
         if play_again == "y":
-            start_game()
+            prob_wakeup_A1_B1()
             exit()
         if play_again == "n":
             print("We'll be here waiting when you change your mind.")
@@ -42,13 +45,14 @@ def prob_wakeup_A1_B1():
     This function starts the game and the scenario where the player wakes up in a cave and must make a choice to find the exit.
     :return: None
     '''
+    print(" ")
     print("You blink awake to the sound of slowly dripping water.")
     print("You look around.")
     print("You are laying on your back in the mouth of a cave.")
     print("Fresh dust settles around you and rubble tumbles off of you as you get up.")
     print("The exit is sealed shut.")
     print(" ")
-    print("If you stay here you will die.")
+    print("If you stay here you will die, you must find the exit.")
     print("1: Feel along the wall of the cave")
     print("2: Follow the smell of food")
     while True:
@@ -63,6 +67,7 @@ def prob_wakeup_A1_B1():
 
 
 def prob_findstuff_A2_C1():
+    print(" ")
     print("You feel along the cave wall. After a few minutes you stumble on something in the dark.")
     print("Crouching down, you can feel the shapes of a rag, a lighter, a stick, and a sharp rock.")
     print("1: Make a spear")
@@ -78,6 +83,7 @@ def prob_findstuff_A2_C1():
         print("Invalid input")
 
 def prob_stillhungry_A3_E1():
+    print(" ")
     print("You smell something in the air as you continue through the cave.")
     print("It smells like fresh meat right before it's roasted on a spit.")
     print("Your stomach growls.")
@@ -98,6 +104,7 @@ def prob_stillhungry_A3_E1():
         print("Invalid input")
 
 def prob_spiderleft_A4_F1_G1():
+    print(" ")
     print("You follow the smell.")
     print("As you turn the corner, you see a gigantic spider busily wrapping what looks like a boar's carcass in webs.")
     print("Its mandibles drip with saliva that hisses as it touches the wrappings.")
@@ -128,6 +135,7 @@ def prob_spiderleft_A4_F1_G1():
         print("Invalid input")
         
 def prob_boarbits_A5_H1():
+    print(" ")
     print("You slash at the spider with your spear.")
     print("It shrieks in pain as you make contact, whipping around to snap its mandibles at you.")
     print("Spittle flecks your cheeks. Its burns and you can feel your skin corroding.")
@@ -176,6 +184,7 @@ def prob_blindattack_H3_J1():
         print("Invalid input")
 
 def prob_cavebugs_C2_D1():
+    print(" ")
     print("The light from the torch attracts a bunch of cave bugs. they start to climb you to try to get to the light")
     print("1: Stay still")
     print("2: Brush them off and run")
@@ -191,6 +200,7 @@ def prob_cavebugs_C2_D1():
         print("Invalid input")
 
 def prob_ladyliberty_C3_D2():
+    print(" ")
     print("They climb up you and jump into the fire, making it bigger you are burnt and hurt, but your torch is now extra bright. it allows you to see a patch of shimmery looking moss")
     print("1: Eat moss")
     print("2: Don't eat moss")
@@ -198,7 +208,7 @@ def prob_ladyliberty_C3_D2():
         selectC3D2 = input("Input 1 or 2:")
         if selectC3D2 == '1':
             print("You get high and doze off to sleep")
-            start_game()
+            prob_wakeup_A1_B1()
             break
         if selectC3D2 == '2':
             print("Don't eat moss and remain hungry, you eventually starve")
@@ -207,6 +217,7 @@ def prob_ladyliberty_C3_D2():
         print("Invalid input")
 
 def prob_spiderright_K1_L1_M1_B2():
+    print(" ")
     print("You encounter a giant cave spider busy wrapping what looks like a boar carcass in webs. its eyes are cloudy with disuse (it is blind)")
     print("1: Try to fight it")
     print("2: Sneak to the left")
@@ -232,6 +243,7 @@ def prob_spiderright_K1_L1_M1_B2():
         print("Invalid input")
 
 def prob_behindyou_L2_N1():
+    print(" ")
     print("You successfully sneak past the spider but you hear something behind you")
     print("1: Run away")
     print("2: Stay and see what it is")
@@ -241,12 +253,13 @@ def prob_behindyou_L2_N1():
             prob_3paths_L3_O1_P1()
             break
         if selectL2N1 == '2':
-            print("It smells you and deems you delicious")
+            print("It approach smells you and deems you delicious")
             you_die()
             break
         print("Invalid input")
 
 def prob_3paths_L3_O1_P1():
+    print(" ")
     print("It hears you running and starts chasing you, you see three pathways you must choose one to go down")
     print("1: Take a left")
     print("2: Go straight")
@@ -267,6 +280,7 @@ def prob_3paths_L3_O1_P1():
         print("Invalid input")
 
 def prob_3morepaths_P2_Q1_R1():
+    print(" ")
     print("You trip and are severely injured but you find 3 more pathways")
     print("1: Steer left")
     print("2: Continue straight")
@@ -288,6 +302,7 @@ def prob_3morepaths_P2_Q1_R1():
         print("Invalid input")
 
 def main():
+    print("welcome to my text adventure game") #edit this to soud good this is the first text to aper to start the game
     prob_wakeup_A1_B1()         # Starts the game by calling the first problem function.
 
 main()
